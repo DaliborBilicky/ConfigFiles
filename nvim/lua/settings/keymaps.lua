f38c19e4320dad2fe08b7959494b556ae951f20e
Dalibor Bilicky <d.bilicky@gmail.com>
2023-12-29 09:31:22 +0100

    Added none-ls plugin for better use

--- nvim/lua/settings/keymaps.lua
+++ nvim/lua/settings/keymaps.lua
@@ -3,7 +3,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-vim.keymap.set("n", "<leader>w", ":w!<CR>")
+local formatOnSave = function()
+	vim.api.nvim_command("lua vim.lsp.buf.format()")
+	vim.api.nvim_command("write!")
+end
+
+vim.keymap.set("n", "<leader>w", formatOnSave)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -16,8 +21,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
-vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
